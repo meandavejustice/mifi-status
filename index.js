cf73bb029774d684a80f69afc16cd3ff22e488fc
Dave Justice <davejustishh@gmail.com>
2016-04-11 11:06:21 -0700

    add connectedDevices info | bump version 2.0.0

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+var request = require('request');
+
 function getStatus(cb) {
-  require('request')('http://my.jetpack/srv/status', function(err, resp, body) {
+  request('http://my.jetpack/srv/status', function(err, resp, body) {
     if (err) cb(err);
     else cb(null, JSON.parse(body).statusData);
   });
 }
 
-module.exports = getStatus;
+function getConnections(cb) {
+  request('http://my.jetpack/connecteddevices/refresh', function(err, resp, body) {
+    if (err) cb(err);
+    else cb(null, JSON.parse(body))
+  });
+}
+
+module.exports.status = getStatus;
+module.exports.connections = getConnections;
